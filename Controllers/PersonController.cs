f4e177de52ee126dbd7cca1308a9e1ca27c6c54a
manwitnoutname <fominykh.tolik@gmail.com>
2019-04-05 12:20:07 +0700

    init

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lr1WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lr1WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class PersonController : ControllerBase
+    {
+        private static List<PersonModel> persons = new List<PersonModel>();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PersonModel>> Get()
+        {
+            return persons;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<PersonModel> Get(int id)
+        {
+            if (persons.Count <= id)
+            {
+                throw new IndexOutOfRangeException("Человека с таким номером анкеты нет!");
+            }
+            return persons[id];
+        }
+
+
+        [HttpPost]
+        public void Post([FromBody] PersonModel personModel)
+        {
+            if (personModel != null)
+            {
+                persons.Add(personModel);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] PersonModel personModel)
+        {
+            if (persons.Count <= id)
+            {
+                throw new IndexOutOfRangeException("Человека с таким номером анкеты нет!");
+            }
+            persons[id] = personModel;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            if (persons.Count <= id)
+            {
+                throw new IndexOutOfRangeException("Человека с таким номером анкеты нет!");
+            }
+            persons.RemoveAt(id);
+        }
+    }
+}
---
+++ Models/PersonModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lr1WebApi.Models
+{
+    public class PersonModel
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string Sex { get; set; }
+        public string Relationships { get; set; }
+    }
+}
